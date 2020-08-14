3731018658d9f6924acef51a0e8298bbe8330f59
5ammuel <sammuel.tracy@gmail.com>
2020-08-14 16:36:48 -0500

    Initial Component Setup

---
+++ Comments.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Comments extends Component {
+	render() {
+		return (
+			<div>
+				<p>The Comments Component is standing by!</p>
+			</div>
+		);
+	}
+}
+export default Comments;
---
+++ Images.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Images extends Component {
+	render() {
+		return (
+			<div>
+				<p>The Images Component is standing by!</p>
+			</div>
+		);
+	}
+}
+export default Images;
