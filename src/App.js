d183d5f181d8de86da1e95034222d85986480cc6
5ammuel <sammuel.tracy@gmail.com>
2020-08-14 15:54:59 -0500

    Install Dependencies/Setup App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 class App extends Component {
 	render() {
 		return (
-      <Router>
-			<div className='container'>
-				<h1>Let's get Salty!</h1>
-			</div>
-      </Router>
+			<Router>
+				<div className='container'>
+					<h1>Let's get Salty!</h1>
+				</div>
+			</Router>
 		);
 	}
 }
