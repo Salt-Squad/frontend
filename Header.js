799ca37f9d83c955e6160cb9eb2a63266c2a1bf2
AngelaT623 <Angela.Taylor87@gmail.com>
2020-08-17 12:07:14 -0400

    edit header.js

--- Header.js
+++ Header.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Header extends Component {
 	render() {
 		return (
-			<div>
-				<p>The Header Component is standing by!</p>
+			<div className='header'>
+				<p>A LIL' BIT SALTY!</p>
 			</div>
 		);
 	}
