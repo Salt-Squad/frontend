39cf1f927f1de987a939cf11a4e7875efa2ec40f
AngelaT623 <Angela.Taylor87@gmail.com>
2020-08-17 19:13:32 -0400

    added dependences

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Header";
 import Images from "./Images";
 import Comments from "./Comments";
+import "./App.css";
 
 class App extends Component {
 	constructor(props) {
