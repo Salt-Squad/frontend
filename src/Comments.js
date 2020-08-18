d14a81738f3036abc064cf6693182ba4dd552bab
5ammuel <sammuel.tracy@gmail.com>
2020-08-18 12:00:35 -0500

    Build comment form in Comments.js

--- src/Comments.js
+++ src/Comments.js
@@ -4,7 +4,11 @@ class Comments extends Component {
 	render() {
 		return (
 			<div>
-				<p>The Comments Component is standing by!</p>
+				<p>Comment Below</p>
+				<form className="commentForm">
+					<input type="text" name="Comment" className="commentField"/>
+					<input type="submit" name="Submit" className="submitButton"/>
+				</form>
 			</div>
 		);
 	}
