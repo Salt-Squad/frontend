63f6fcc8fd62e64d843f9a4ea1000f1e9d399fc8
AngelaT623 <Angela.Taylor87@gmail.com>
2020-08-19 07:33:15 -0400

    added axios, testing route paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Link } from 'react-router-dom';
 import Header from "./Header";
 import Images from "./Images";
 import Comments from "./Comments";
@@ -14,14 +13,10 @@ class App extends Component {
 	render() {
 		return (
 			<div className='container'>
-				<Router>
-					<h2>Salty is alive</h2>
-					<main>
-						<Header />,
-						<Images />,
-						<Comments />,
-					</main>
-				</Router>
+				<h2>Salty is alive</h2>
+				<Route exact path='/' component={Header} />
+				<Route path='/api/images' component={Images} />
+				<Route path='/api/images' component={Comments} />
 			</div>
 		);
 	}
