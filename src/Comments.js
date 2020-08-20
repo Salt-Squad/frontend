f0f0b6cfcbfa0c90123d579c287e225c1f490ae8
AngelaT623 <Angela.Taylor87@gmail.com>
2020-08-19 20:35:05 -0400

    fixed comments.js for render data

--- src/Comments.js
+++ src/Comments.js
@@ -1,38 +1,46 @@
 import React, { Component } from 'react';
-import axios from "axios";
-
+import axios from 'axios';
 class Comments extends Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 		this.state = {
-			images: [],
+			name: '',
+			url: ' ',
 		};
 	}
-
-	handleSubmit = (event) => {
-		event.preventDefault();
-		const url = "localhost:3000/api/images/new"
-		axios.post(url, this.state.name);
+	onChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
 	};
-
-	handleChange = (event) => {
-		this.setState({ title: event.target.value });
+	onSubmit = (e) => {
+		e.preventDefault();
+		console.log(this.state);
+		axios
+			.post('/api/images/new', this.state)
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 	};
-
 	render() {
 		return (
 			<div>
 				<p>Comment Below</p>
-				<form className='submitForm'>
+				<form className='commentForm' onSubmit={this.onSubmit}>
 					<input
 						type='text'
-						name='title'
-						className='titleField'
-						onChange={this.handleChange}
-						value={this.state.title}
+						name='name'
+						value={this.state.name} 
+						onChange={this.onChange}
+					 ></input>
+					<input
+						type='text'
+						name='url'
+						value={this.state.url}
+						onChange={this.onChange}
 					/>
-					<input type='text' name='urlLink' className='urlField' />
-					<input type='submit' name='Submit' className='submitButton' />
+					<button type='submit'>submit</button>
 				</form>
 			</div>
 		);
